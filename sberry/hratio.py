4c30c37f1f4ddda5bc7d29179974f3e9989d2936
rvicedomini <ric.vice@protonmail.com>
2021-03-12 15:24:02 +0100

    created a class for computing average Hamming ratio of a separation, data is stored at the phaseset level

--- sberry/hratio.py
+++ sberry/hratio.py
@@ -15,78 +15,98 @@ class CondensedRead:
         self.positions = []
         self.seq = []
 
+class AverageHammingRatio:
+    
+    def __init__(self):
+        self.ahrdict = {}
 
-def hamming_ratio(str_a,str_b):
-    assert len(str_a)==len(str_b) and len(str_a)>0
-    return sum(a!=b for a,b in zip(str_a,str_b))/len(str_a)
+    def _hamming_ratio(self,str_a,str_b):
+        assert len(str_a)==len(str_b) and len(str_a)>0
+        return sum(a!=b for a,b in zip(str_a,str_b))/len(str_a)
 
-
-def average_contig_hratio( bamfile, ctg, ps_list, min_read_overlap=3000):
-
-    with pysam.AlignmentFile(bamfile,'rb') as bam:
+    def _average_contig_hratio(self, bamfile, ctg, ps_list, min_read_overlap=3000):
+        with pysam.AlignmentFile(bamfile,'rb') as bam:
+            for phaseset in ps_list:
+                ps_start=phaseset.start()
+                ps_end=phaseset.end()
+                ps_positions=set(phaseset.positions)
+                phaseset.creads=defaultdict(CondensedRead)
+                for pileupcol in bam.pileup(reference=ctg, start=ps_start, stop=ps_end, min_base_quality=0, ignore_overlaps=False, stepper='all'):
+                    pos=pileupcol.reference_pos
+                    if pos not in ps_positions: # FIXME: this could be done more efficiently
+                        continue
+                    for pileupread in pileupcol.pileups:
+                        alignment=pileupread.alignment
+                        nuc = alignment.query_sequence[pileupread.query_position] if not pileupread.is_del and not pileupread.is_refskip else '-'
+                        read_name=alignment.query_name
+                        read_overlap=min(ps_end,alignment.reference_end)-max(ps_start,alignment.reference_start)
+                        assert read_overlap > 0
+                        if read_overlap >= min(min_read_overlap,ps_end-ps_start):
+                            if len(phaseset.creads[read_name].positions) == 0 or phaseset.creads[read_name].positions[-1] != pos:
+                                phaseset.creads[read_name].positions.append(pos)
+                                phaseset.creads[read_name].seq.append(nuc)
+        ps_hratios=[]
         for phaseset in ps_list:
-            ps_start=phaseset.start()
-            ps_end=phaseset.end()
-            ps_positions=set(phaseset.positions)
-            phaseset.creads=defaultdict(CondensedRead)
-            for pileupcol in bam.pileup(reference=ctg, start=ps_start, stop=ps_end, min_base_quality=0, ignore_overlaps=False, stepper='all'):
-                pos=pileupcol.reference_pos
-                if pos not in ps_positions: # FIXME: this could be done more efficiently
+            ps_hratio_sum=0
+            ps_hratio_count=0
+            creads=phaseset.creads
+            for read_name,cread in creads.items():
+                cread_positions = set(cread.positions)
+                assert len(cread_positions) == len(cread.positions)
+                ps_indices = [ i for i,pos in enumerate(phaseset.positions) if pos in cread_positions ]
+                ps_haplotypes = { h:''.join(htype[i] for i in ps_indices) for h,htype in phaseset.haplo.items() }
+                cread_seq = ''.join(cread.seq)
+                ps_hratio_sum += min(self._hamming_ratio(cread_seq,hap_seq) for hap_seq in ps_haplotypes.values())
+                ps_hratio_count += 1
+            ps_hratio_avg = ps_hratio_sum/ps_hratio_count if ps_hratio_count > 0 else 1.0
+            ps_hratios.append((phaseset.psid,ps_hratio_avg,ps_hratio_count))
+        return (ctg,ps_hratios)
+
+    def _aggregate_contig_hratios(self,result):
+        ctg,ctg_ps_hratios=result
+        self.ahrdict[ctg]={}
+        for psid,ps_hratio_avg,ps_hratio_count in ctg_ps_hratios:
+            self.ahrdict[ctg][psid]=(ps_hratio_avg,ps_hratio_count)
+
+    def reference_average_hratio(self,reference):
+        ref_hratio_avg=1.0
+        ref_hratio_count=0
+        if reference in self.ahrdict:
+            for ps_hratio,ps_count in self.ahrdict[reference].values():
+                if ps_count == 0:
                     continue
-                for pileupread in pileupcol.pileups:
-                    alignment=pileupread.alignment
-                    nuc = alignment.query_sequence[pileupread.query_position] if not pileupread.is_del and not pileupread.is_refskip else '-'
-                    read_name=alignment.query_name
-                    read_overlap=min(ps_end,alignment.reference_end)-max(ps_start,alignment.reference_start)
-                    assert read_overlap > 0
-                    if read_overlap >= min(min_read_overlap,ps_end-ps_start):
-                        if len(phaseset.creads[read_name].positions) == 0 or phaseset.creads[read_name].positions[-1] != pos:
-                            phaseset.creads[read_name].positions.append(pos)
-                            phaseset.creads[read_name].seq.append(nuc)
-    
-    ctg_hratio=defaultdict(list)
-    for phaseset in ps_list:
-        creads=phaseset.creads
-        for read_name,cread in creads.items():
-            cread_positions = set(cread.positions)
-            assert len(cread_positions) == len(cread.positions)
-            ps_indices = [ i for i,pos in enumerate(phaseset.positions) if pos in cread_positions ]
-            ps_haplotypes = { h:''.join(htype[i] for i in ps_indices) for h,htype in phaseset.haplo.items() }
-            cread_seq = ''.join(cread.seq)
-            hratio = min(hamming_ratio(cread_seq,hap_seq) for hap_seq in ps_haplotypes.values())
-            ctg_hratio[phaseset.psid].append(hratio)
-    
-    return (ctg,ctg_hratio)
+                ref_hratio_avg = (ref_hratio_avg*ref_hratio_count + ps_hratio*ps_count)/(ref_hratio_count+ps_count)
+                ref_hratio_count = ref_hratio_count + ps_count
+        return (ref_hratio_avg,ref_hratio_count)
 
+    def phaseset_average_hratio(self,reference,psid):
+        if reference in self.ahrdict and psid in self.ahrdict[reference]:
+            return self.ahrdict[reference][psid]
+        return (1.0,0)
 
-def average_hratio( bamfile:str, psc:PhasesetCollection, nproc:int = 1 ):
+    def compute( self, bamfile:str, psc:PhasesetCollection, nproc:int = 1 ):
 
-    references = psc.references()
+        self.ahrdict = {}
+        references = psc.references()
+        
+        pool = multiprocessing.Pool(processes=nproc,maxtasksperchild=1)
+        for ctg in references:
+            ctg_phasesets = psc.phasesets(ctg)
+            pool.apply_async(self._average_contig_hratio, args=(bamfile,ctg,ctg_phasesets,), callback=self._aggregate_contig_hratios)
+        pool.close()
+        pool.join()
 
-    results=[]
-    def get_result(res):
-        ctg,ctg_hratio=res
-        for ps_id,hr_list in ctg_hratio.items():
-            nreads = len(hr_list)
-            avg_hratio = sum(hr_list)/nreads if nreads > 0 else 0.0
-            results.append( (ctg,ps_id,avg_hratio,nreads) )
-    
-    pool = multiprocessing.Pool(processes=nproc,maxtasksperchild=1)
-    for ctg in references:
-        ctg_phasesets = psc.phasesets(ctg)
-        pool.apply_async(average_contig_hratio, args=(bamfile,ctg,ctg_phasesets,), callback=get_result)
-    pool.close()
-    pool.join()
-
-    tot_nreads=0
-    weighted_sum=0
-    for contig,psid,hratio,nreads in results:
-        weighted_sum+=hratio*nreads
-        tot_nreads+=nreads
-    
-    num_phasesets = psc.size()
-    avg_hratio = weighted_sum/tot_nreads if tot_nreads > 0 else 1.0
-    return (avg_hratio,num_phasesets,tot_nreads)
+        hratio_avg=1.0
+        hratio_size=0
+        for reference in self.ahrdict:
+            ref_hratio,ref_size=self.reference_average_hratio(reference)
+            if ref_size == 0:
+                continue
+            hratio_avg = (hratio_avg*hratio_size + ref_hratio*ref_size)/(hratio_size+ref_size)
+            hratio_size = hratio_size + ref_size
+        
+        num_phasesets = psc.size()
+        return (hratio_avg,num_phasesets,hratio_size)
     
 #    print(f'Writing results to {opt.prefix}.hratio.tsv',file=sys.stderr)
 #    with open(f'{opt.prefix}.hratio.tsv','w') as out:
