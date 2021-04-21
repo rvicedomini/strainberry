28f0b9fbf55ace0bc866910f6da367334e0b55c1
rvicedomini <ric.vice@protonmail.com>
2021-04-21 17:58:09 +0200

    added function to compute simple assembly metrics from a fasta index

--- sberry/utils.py
+++ sberry/utils.py
@@ -7,6 +7,30 @@ def reverse_complement(seq):
     return seq.translate(rctable)[::-1]
 
 
+# input is the path to a fasta index 
+# (i.e., .fai file generated with samtools faidx)
+# output is a tuple -> assembly_size, sequence_number, assembly_N50
+def faidx_stats(fname):
+    seqlen_list = []
+    with open(fname,'r') as fai:
+        for line in fai:
+            _,seqlen,_=line.split('\t',2)
+            seqlen_list.append(int(seqlen))
+    seqlen_list.sort(reverse=True)
+    
+    assembly_size = sum(seqlen_list)
+    assembly_nseq = len(seqlen_list)
+    assembly_largest = seqlen_list[0] if assembly_nseq>0 else 0
+    assembly_n50 = 0
+    partial_sum = 0
+    for seqlen in seqlen_list:
+        partial_sum += seqlen
+        if 2*partial_sum >= assembly_size:
+            assembly_n50 = seqlen
+            break
+    return assembly_size,assembly_nseq,assembly_n50
+    
+
 def insert_newlines(inString, every=80):
     return inString if every <= 0 else '\n'.join(inString[i:i+every] for i in range(0, len(inString), every))
 
